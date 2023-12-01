fedf4d0c034a4187b6fa92f3dd8a2e3fc1cce8af
CombineSlayer24 <luvangel157@aol.com>
2023-12-01 15:42:08 -0700

    Fire stuff and ceda, and added shine to commons

--- lua/entities/z_common.lua
+++ lua/entities/z_common.lua
@@ -15,23 +15,23 @@ ENT.IsClimbing = false
 local ENT_CommonFiles = file.Find( "left4dead/z_common/*", "LUA", "nameasc" )
 
 for k, luafile in ipairs( ENT_CommonFiles ) do
-    if string.StartWith( luafile, "sv_" ) then -- Server Side Files
-        include( "left4dead/z_common/" .. luafile )
-        print( "Left 4 Dead Common Infected ENT TABLE: Included Server Side ENT Lua File [" .. luafile .. "]" )
-    elseif string.StartWith( luafile, "sh_" ) then -- Shared Files
-        if SERVER then
-            AddCSLuaFile( "left4dead/z_common/" .. luafile )
-        end
-        include( "left4dead/z_common/" .. luafile )
-        print( "Left 4 Dead Common Infected ENT TABLE: Included Shared ENT Lua File [" .. luafile .. "]" )
-    elseif string.StartWith( luafile, "cl_" ) then -- Client Side Files
-        if SERVER then
-            AddCSLuaFile( "left4dead/z_common/" .. luafile )
-        else
-            include( "left4dead/z_common/" .. luafile )
-            print( "Left 4 Dead Common Infected ENT TABLE: Included Client Side ENT Lua File [" .. luafile .. "]" )
-        end
-    end
+	if string.StartWith( luafile, "sv_" ) then -- Server Side Files
+		include( "left4dead/z_common/" .. luafile )
+		print( "Left 4 Dead Common Infected ENT TABLE: Included Server Side ENT Lua File [" .. luafile .. "]" )
+	elseif string.StartWith( luafile, "sh_" ) then -- Shared Files
+		if SERVER then
+			AddCSLuaFile( "left4dead/z_common/" .. luafile )
+		end
+		include( "left4dead/z_common/" .. luafile )
+		print( "Left 4 Dead Common Infected ENT TABLE: Included Shared ENT Lua File [" .. luafile .. "]" )
+	elseif string.StartWith( luafile, "cl_" ) then -- Client Side Files
+		if SERVER then
+			AddCSLuaFile( "left4dead/z_common/" .. luafile )
+		else
+			include( "left4dead/z_common/" .. luafile )
+			print( "Left 4 Dead Common Infected ENT TABLE: Included Client Side ENT Lua File [" .. luafile .. "]" )
+		end
+	end
 end
 
 -- This might seem reduntant, but trust me, it's not.
@@ -74,38 +74,38 @@ if CLIENT then language.Add( "nb_common_infected", ENT.PrintName ) end
 
 local ci_BatonModels = 
 {
-    ["models/infected/c_nb/common_male_police01.mdl"] = true,
-    ["models/infected/c_nb/trs_common_male_police01.mdl"] = true
+	["models/infected/c_nb/common_male_police01.mdl"] = true,
+	["models/infected/c_nb/trs_common_male_police01.mdl"] = true
 }
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:SetUpZombie()
 	-- When it comes down to models, we make all of the models available for the player/admins to customize.
 	-- Sorta similar to how the population is handled in L4D2.
 	-- Should the player only want Military, Police, Rural, Common, Hospital, or all of above? ect ect.
-    local mdls = {}
-    for k, v in pairs( Z_MaleModels ) do table_insert( mdls, v ) end
-    for k, v in pairs( Z_FemaleModels ) do table_insert( mdls, v ) end
+	local mdls = {}
+	for k, v in pairs( Z_MaleModels ) do table_insert( mdls, v ) end
+	for k, v in pairs( Z_FemaleModels ) do table_insert( mdls, v ) end
 
-    -- Now select a random model from the combined table
-    local spawnMdl = mdls[ random( #mdls ) ]
-    self:SetModel( spawnMdl )
+	-- Now select a random model from the combined table
+	local spawnMdl = mdls[ random( #mdls ) ]
+	self:SetModel( spawnMdl )
 
-    -- Set Gender based on model
-    if table_HasValue( Z_MaleModels, spawnMdl ) then
-        self.Gender = "Male"
-    elseif table_HasValue( Z_FemaleModels, spawnMdl ) then
-        self.Gender = "Female"
-    end
+	-- Set Gender based on model
+	if table_HasValue( Z_MaleModels, spawnMdl ) then
+		self.Gender = "Male"
+	elseif table_HasValue( Z_FemaleModels, spawnMdl ) then
+		self.Gender = "Female"
+	end
 
-    -- from Lambdaplayers
-    for _, v in ipairs( self:GetBodyGroups() ) do
-        local subMdls = #v.submodels
-        if subMdls == 0 then continue end
-        self:SetBodygroup( v.id, random( 0, subMdls ) )
-    end
+	-- from Lambdaplayers
+	for _, v in ipairs( self:GetBodyGroups() ) do
+		local subMdls = #v.submodels
+		if subMdls == 0 then continue end
+		self:SetBodygroup( v.id, random( 0, subMdls ) )
+	end
 
-    local skinCount = self:SkinCount()
-    if skinCount > 0 then self:SetSkin( random( 0, skinCount - 1 ) ) end
+	local skinCount = self:SkinCount()
+	if skinCount > 0 then self:SetSkin( random( 0, skinCount - 1 ) ) end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:Initialize()
@@ -130,16 +130,14 @@ function ENT:Initialize()
 			end
 		end
 
-		--self.loco:SetAcceleration( random( 250, 600 ) )
-		self.loco:SetDeceleration( 200 )
 		self.loco:SetStepHeight( 18 )
 		self.loco:SetGravity( sv_gravity:GetFloat() )
 
 		self:SetCollisionBounds( collisionmins, collisionmaxs )
 		self:PhysicsInitShadow()
-		self:SetSolid(SOLID_BBOX)
+		self:SetSolid( SOLID_BBOX )
 
-		self:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE)
+		self:SetCollisionGroup( COLLISION_GROUP_INTERACTIVE )
 
 		self:SetLagCompensated( true )
 		self:AddFlags( FL_OBJECT + FL_NPC )
@@ -229,12 +227,20 @@ function ENT:OnKilled( dmginfo )
 	ragdoll:SetCollisionGroup( COLLISION_GROUP_DEBRIS )
 
 	self:SetDeathExpression( ragdoll )
-
-	if IsValid( self.item ) then
-		self:CreateItemOnDeath( ragdoll )
-	end
+	if IsValid( self.item ) then self:CreateItemOnDeath( ragdoll ) end
 
 	self:Vocalize( ZCommon_L4D1_Death )
+
+	-- Suit deflate sound
+	if self:IsUnCommonInfected( "CEDA" ) then
+		ragdoll:EmitSound("left4dead/vocals/infected/death/ceda_suit_deflate_0" .. random( 3 ) .. ".wav", 75, random( 90, 100 ), 0.75 )
+	end
+
+	if dmginfo:IsDamageType( DMG_BURN ) then
+		local burnMat = "models/left4dead/ci_burning"
+		ragdoll:Ignite( 5, 0 )
+		ragdoll:SetMaterial( burnMat )
+	end
 
 	-- Have a chance for the ragdoll to fly around a bit.
 	-- Instead of faceplanting if no animations played, they could land
@@ -253,7 +259,48 @@ function ENT:OnKilled( dmginfo )
 		end
 	end
 end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:IsFlameproof( dmginfo )
+    if dmginfo:IsDamageType( DMG_BURN ) then
+		dmginfo:SetDamage( 0 )
+		
+        if SERVER then
+            self:Extinguish()
+            self:StopSound( "General.BurningFlesh" )
+            if dmginfo:GetAttacker():GetClass() == "entityflame" then
+                self:StopSound( "General.BurningObject" )
+                dmginfo:GetAttacker():Fire( "kill", "", 0.1 )
+            end
+        end
 
+        return true
+    end
+
+    return false
+end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:OnInjured(dmginfo)
+    -- If uncommon infected are either CEDA, Fallen or Jimmy Gibbs Jr.
+	-- will become flameproof
+    if self:IsUnCommonInfected( "CEDA" ) or self:IsUnCommonInfected( "FALLEN" ) or self:IsUnCommonInfected( "JIMMYGIBBS" )  then 
+        self:IsFlameproof( dmginfo )
+    end
+
+	-- Insta kill CI if on fire
+    if dmginfo:IsDamageType( DMG_BURN ) then
+        dmginfo:SetDamage( 20 )
+	end
+
+	-- Play Bullet impact sounds
+	if !dmginfo:IsDamageType( DMG_BURN ) and self:Health() > 0 and random( 5 ) == 1 then
+		self:Vocalize( Zombie_BulletImpact )
+	end
+
+	-- Play pain sounds
+	if random( 3 ) == 1 then
+		self:Vocalize( ZCommon_Pain )
+	end
+end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:Draw()
 	self.ci_lastdraw = RealTime() + 0.1
@@ -264,7 +311,8 @@ end
 function ENT:OnRemove()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function ENT:DirectPoseParametersAt(pos, pitch, yaw, center)
+-- From DRGBase, function updated
+function ENT:DirectPoseParametersAt( pos, pitch, yaw, center )
 	local isstring = isstring
 	local isentity = isentity
 	local isvector = isvector
@@ -273,17 +321,17 @@ function ENT:DirectPoseParametersAt(pos, pitch, yaw, center)
 	local SetPoseParameter = self.SetPoseParameter
 	local WorldSpaceCenter = self.WorldSpaceCenter
 
-	if not isstring(yaw) then
-		return self:DirectPoseParametersAt(pos, pitch.."_pitch", pitch.."_yaw", yaw)
-	elseif isentity(pos) then 
+	if !isstring( yaw ) then
+		return self:DirectPoseParametersAt( pos, pitch.."_pitch", pitch.."_yaw", yaw )
+	elseif isentity( pos ) then 
 		pos = pos:WorldSpaceCenter() 
 	end
 
 	if isvector(pos) then
-		center = center or WorldSpaceCenter(self)
-		local angle = (pos - center):Angle()
-		SetPoseParameter(self, pitch, AngleDifference(angle.p, GetAngles(self).p))
-		SetPoseParameter(self, yaw, AngleDifference(angle.y, GetAngles(self).y))
+		center = center or WorldSpaceCenter( self )
+		local angle = ( pos - center ):Angle()
+		SetPoseParameter( self, pitch, AngleDifference( angle.p, GetAngles( self ).p ) )
+		SetPoseParameter( self, yaw, AngleDifference( angle.y, GetAngles( self ).y ) )
 	else
 		SetPoseParameter(self, pitch, 0)
 		SetPoseParameter(self, yaw, 0)
@@ -292,14 +340,14 @@ end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:LookAtEntity( ent )
 	local enemy = self:FindNearestEnemy()
-	if IsValid(enemy) and enemy:GetBonePosition(1) then
-		local distance = self:GetPos():Distance(enemy:GetPos())
+	if IsValid( enemy ) and enemy:GetBonePosition( 1 ) then
+		local distance = self:GetPos():Distance( enemy:GetPos() )
 
 		-- Look at out prey
 		if distance < 700 then
-			self:DirectPoseParametersAt(enemy:GetBonePosition(1), "body", self:EyePos())
+			self:DirectPoseParametersAt( enemy:GetBonePosition( 1 ), "body", self:EyePos() )
 		else -- If our prey is too far, don't look at them
-			self:DirectPoseParametersAt(nil, "body_pitch", "body_yaw", 0)
+			self:DirectPoseParametersAt( nil, "body_pitch", "body_yaw", 0 )
 		end
 	end
 end
@@ -310,11 +358,11 @@ function ENT:HandleStuck()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:FindNearestEnemy()
-    local enemies = ents_FindInSphere(self:GetPos(), 2500) -- Find potential targets
-    local nearestEnemy = nil -- Our enemy
-    local nearestDistance = MathHuge -- Limited to out range of FindInSphere, 
+	local enemies = ents_FindInSphere(self:GetPos(), 2500) -- Find potential targets
+	local nearestEnemy = nil -- Our enemy
+	local nearestDistance = MathHuge -- Limited to out range of FindInSphere, 
 
-    for _, enemy in pairs(enemies) do
+	for _, enemy in pairs(enemies) do
 
 		if enemy ~= self and not enemy.IsCommonInfected and enemy:IsNPC() or enemy:IsPlayer() or !ignorePlys or enemy:IsNextBot() and not enemy.IsCommonInfected then
 			local distance = self:GetPos():Distance(enemy:GetPos()) -- Calculating distance between zombie and target
@@ -324,24 +372,24 @@ function ENT:FindNearestEnemy()
 				nearestEnemy = enemy
 			end
 		end
-    end
+	end
 
-    return nearestEnemy -- Returning target for all purposes.
+	return nearestEnemy -- Returning target for all purposes.
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:RunBehaviour()
 	while ( true ) do
 		local enemy = self:FindNearestEnemy()
-		if not IsValid(enemy) then
+		if not IsValid( enemy ) then
 			self:StartWandering()
-		elseif IsValid(enemy) then
-			local distance = self:GetPos():Distance(enemy:GetPos())
+		elseif IsValid( enemy ) then
+			local distance = self:GetPos():Distance( enemy:GetPos() )
 			if distance > 100 then
 				self:ChaseTarget(enemy)
-				--PrintMessage(HUD_PRINTTALK, "Chasing!")
+				PrintMessage(HUD_PRINTTALK, "Chasing!")
 			else
 				self:Attack(enemy)
-				--PrintMessage(HUD_PRINTTALK, "Attacking!")
+				PrintMessage(HUD_PRINTTALK, "Attacking!")
 			end
 		end
 		coroutine.yield()
@@ -350,8 +398,8 @@ end
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:StartWandering()
-	self.loco:SetAcceleration(5000)
-	self.loco:SetDeceleration(5000)
+	self.loco:SetAcceleration( 5000 )
+	self.loco:SetDeceleration( 5000 )
 
 	local anim = self:GetActivity()
 	
@@ -361,9 +409,9 @@ function ENT:StartWandering()
 		anim = "ACT_TERROR_RUN_INTENSE"
 	end
 
-	self:ResetSequence(anim)
-	self.loco:SetDesiredSpeed(350)
-	self:MoveToPos(self:GetPos() + VectorRand() * math.random(250, 300))
+	self:ResetSequence( anim )
+	self.loco:SetDesiredSpeed( 350 )
+	self:MoveToPos( self:GetPos() + VectorRand() * math.random( 250, 300 ) )
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:Attack(target)
@@ -424,31 +472,31 @@ function ENT:ChaseTarget(target)
 	self.loco:SetDesiredSpeed(300)
 	self:LookAtEntity()
 
-    local TargetToChase = target
-    local path = Path("Follow")
-    --path:Chase(self, target)
-    path:Compute(self, TargetToChase:GetPos())
-    while (path:IsValid() and IsValid(TargetToChase)) do
-        if path:GetAge() > 0.1 then
-            path:Compute(self, TargetToChase:GetPos())
-        end
+	local TargetToChase = target
+	local path = Path("Follow")
+	--path:Chase(self, target)
+	path:Compute(self, TargetToChase:GetPos())
+	while (path:IsValid() and IsValid(TargetToChase)) do
+		if path:GetAge() > 0.1 then
+			path:Compute(self, TargetToChase:GetPos())
+		end
 
 		if developer:GetBool() then path:Draw() end
 
-        path:Update(self)
+		path:Update(self)
 
-		local distance = self:GetPos():Distance(TargetToChase:GetPos())
-        if distance > 90 then
-            break
-        end
+		local distance = self:GetPos():Distance( TargetToChase:GetPos() )
+		if distance > 90 then
+			break
+		end
 
-        coroutine.yield()
-    end
+		coroutine.yield()
+	end
 end
 
 function ENT:PlayAttackAnimation()
-	coroutine.wait(0.4)
-	self:ResetSequence("ACT_TERROR_ATTACK_CONTINUOUSLY")
+	coroutine.wait( 0.4 )
+	self:ResetSequence( "ACT_TERROR_ATTACK_CONTINUOUSLY" )
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 --
@@ -519,33 +567,33 @@ end
 --
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:BodyUpdate()
-    local velocity = self.loco:GetVelocity()
-    if !velocity:IsZero() then
-        -- Apparently NEXTBOT:BodyMoveXY() really don't likes swimming animations and sets their playback rate to crazy values, causing the game to crash
-        -- So instead I tried to recreate what that function does, but with clamped set playback rate
-        if self:GetWaterLevel() >= 2 then
-            local selfPos = self:GetPos()
+	local velocity = self.loco:GetVelocity()
+	if !velocity:IsZero() then
+		-- Apparently NEXTBOT:BodyMoveXY() really don't likes swimming animations and sets their playback rate to crazy values, causing the game to crash
+		-- So instead I tried to recreate what that function does, but with clamped set playback rate
+		if self:GetWaterLevel() >= 2 then
+			local selfPos = self:GetPos()
 
-            -- Setup pose parameters (model's legs movement)
-            local moveDir = ( ( selfPos + velocity ) - selfPos ); moveDir.z = 0
-            local moveXY = ( self:GetAngles() - moveDir:Angle() ):Forward()
+			-- Setup pose parameters (model's legs movement)
+			local moveDir = ( ( selfPos + velocity ) - selfPos ); moveDir.z = 0
+			local moveXY = ( self:GetAngles() - moveDir:Angle() ):Forward()
 
-            local frameTime = FrameTime()
-            self:SetPoseParameter( "move_x", Lerp( 15 * frameTime, self:GetPoseParameter( "move_x" ), moveXY.x ) )
-            self:SetPoseParameter( "move_y", Lerp( 15 * frameTime, self:GetPoseParameter( "move_y" ), moveXY.y ) )
+			local frameTime = FrameTime()
+			self:SetPoseParameter( "move_x", Lerp( 15 * frameTime, self:GetPoseParameter( "move_x" ), moveXY.x ) )
+			self:SetPoseParameter( "move_y", Lerp( 15 * frameTime, self:GetPoseParameter( "move_y" ), moveXY.y ) )
 			--self:InvalidateBoneCache()
 
-            -- Setup swimming animation's clamped playback rate
-            local length = velocity:Length()
-            local groundSpeed = self:GetSequenceGroundSpeed( self:GetSequence() )
-            self:SetPlaybackRate( Clamp( ( length > 0.2 and ( length / groundSpeed ) or 1 ), 0.5, 2 ) )
-        else
-            self:BodyMoveXY()
-            return
-        end
-    end
+			-- Setup swimming animation's clamped playback rate
+			local length = velocity:Length()
+			local groundSpeed = self:GetSequenceGroundSpeed( self:GetSequence() )
+			self:SetPlaybackRate( Clamp( ( length > 0.2 and ( length / groundSpeed ) or 1 ), 0.5, 2 ) )
+		else
+			self:BodyMoveXY()
+			return
+		end
+	end
 
-    self:FrameAdvance()
+	self:FrameAdvance()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:SetDeathExpression( ragdoll )
--- lua/left4dead/z_common/sh_util.lua
+++ lua/left4dead/z_common/sh_util.lua
@@ -8,101 +8,118 @@ local timer_Create = timer.Create
 local timer_Adjust = timer.Adjust
 local timer_Remove = timer.Remove
 local IsValid = IsValid
+local ipairs = ipairs
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- expressionType must be typed in as ("idle" and "angry")
 function ENT:ZombieExpression( expressionType )
-    timer_Remove( "AnimationLayer" )
-    timer_Create( "AnimationLayer", 2, 0, function()
-        if !IsValid( self ) then return end
+	timer_Remove( "AnimationLayer" )
+	timer_Create( "AnimationLayer", 2, 0, function()
+		if !IsValid( self ) then return end
 
-        if SERVER then
-            local anim = self:LookupSequence( "exp_" .. expressionType .. "_0" .. random( 6 ) )
-            self:AddGestureSequence( anim, false )
-        end
+		if SERVER then
+			local anim = self:LookupSequence( "exp_" .. expressionType .. "_0" .. random( 6 ) )
+			self:AddGestureSequence( anim, false )
+		end
 
-        timer_Adjust( "AnimationLayer", self:SequenceDuration( anim ) - 0.2 )
-    end)
+		timer_Adjust( "AnimationLayer", self:SequenceDuration( anim ) - 0.2 )
+	end)
 end
+---------------------------------------------------------------------------------------------------------------------------------------------
+function ENT:IsUnCommonInfected( model )
+	local modelTable = {
+		CEDA = { "models/infected/l4d2_nb/uncommon_male_ceda.mdl" },
+	}
 
+	if modelTable[ model ] then
+		for _, v in ipairs( modelTable[ model ] ) do
+			if self:GetModel() == v then
+				return true
+			end
+		end
+	end
+
+	-- Our specific model isn't a uncommon
+	return false
+end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Play the requested voiceline
 -- Action = the global voiceset
 -- Need to make a check for IsSpeaking
 function ENT:Vocalize( action )
-    local pitch
-    local Snd = action[ random( #action ) ]
+	local pitch
+	local Snd = action[ random( #action ) ]
 
-    if self.Gender == "Female" then
-        pitch = random( 100, 112 )
-        self:EmitSound( Snd, 80, pitch )
-    elseif self.Gender == "Male" then
-        pitch = random( 94, 104 )
-        self:EmitSound( Snd, 80, pitch )
-    end
+	if self.Gender == "Female" then
+		pitch = random( 100, 112 )
+		self:EmitSound( Snd, 80, pitch )
+	elseif self.Gender == "Male" then
+		pitch = random( 94, 104 )
+		self:EmitSound( Snd, 80, pitch )
+	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Play the requested animation sequence
 function ENT:PlaySequence( sequence )
-    local seq = self:LookupSequence( sequence )
-    if seq > 0 then
-        self:ResetSequence( seq )
-        self:SetSequence( seq )
-    end
+	local seq = self:LookupSequence( sequence )
+	if seq > 0 then
+		self:ResetSequence( seq )
+		self:SetSequence( seq )
+	end
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Returns the position and angle of a specified bone
 function ENT:GetBoneTransformation( bone, target )
-    target = ( target or self )
+	target = ( target or self )
 
-    local pos, ang = target:GetBonePosition( bone )
-    if !pos or pos:IsZero() or pos == target:GetPos() then
-        local matrix = target:GetBoneMatrix( bone )
-        if matrix and ismatrix( matrix ) then
-            pos = matrix:GetTranslation()
-            ang = matrix:GetAngles()
-        end
-    end
-    
-    return { Pos = pos, Ang = ang, Bone = bone }
+	local pos, ang = target:GetBonePosition( bone )
+	if !pos or pos:IsZero() or pos == target:GetPos() then
+		local matrix = target:GetBoneMatrix( bone )
+		if matrix and ismatrix( matrix ) then
+			pos = matrix:GetTranslation()
+			ang = matrix:GetAngles()
+		end
+	end
+	
+	return { Pos = pos, Ang = ang, Bone = bone }
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Returns a table that contains a position and angle with the specified type. hand or eyes
 local eyeOffAng = Angle( 20, 0, 0 )
 function ENT:GetAttachmentPoint( pointtype )
-    local attachData = { Pos = self:WorldSpaceCenter(), Ang = self:GetForward():Angle(), Index = 0 }
+	local attachData = { Pos = self:WorldSpaceCenter(), Ang = self:GetForward():Angle(), Index = 0 }
 
-    if pointtype == "hand" then
-        local lookup = self:LookupAttachment( "anim_attachment_RH" )
-        local handAttach = self:GetAttachment( lookup )
+	if pointtype == "hand" then
+		local lookup = self:LookupAttachment( "anim_attachment_RH" )
+		local handAttach = self:GetAttachment( lookup )
 
-        if !handAttach then
-            local bone = self:LookupBone( "ValveBiped.Bip01_R_Hand" )
-            if isnumber( bone ) then attachData = self:GetBoneTransformation( bone ) end
-        else
-            attachData = handAttach
-            attachData.Index = lookup
-        end
-    elseif pointtype == "eyes" then
-        local lookup = self:LookupAttachment( "eyes" )
-        local eyeAttach = self:GetAttachment( lookup )
+		if !handAttach then
+			local bone = self:LookupBone( "ValveBiped.Bip01_R_Hand" )
+			if isnumber( bone ) then attachData = self:GetBoneTransformation( bone ) end
+		else
+			attachData = handAttach
+			attachData.Index = lookup
+		end
+	elseif pointtype == "eyes" then
+		local lookup = self:LookupAttachment( "eyes" )
+		local eyeAttach = self:GetAttachment( lookup )
 
-        if !eyeAttach then 
-            attachData.Pos = ( attachData.Pos + vector_up * 30 )
-            attachData.Ang = ( attachData.Ang + eyeOffAng )
-        else
-            attachData = eyeAttach
-            attachData.Index = lookup
-        end
-    end
+		if !eyeAttach then 
+			attachData.Pos = ( attachData.Pos + vector_up * 30 )
+			attachData.Ang = ( attachData.Ang + eyeOffAng )
+		else
+			attachData = eyeAttach
+			attachData.Index = lookup
+		end
+	end
 
-    return attachData
+	return attachData
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 if SERVER then
-    ---------------------------------------------------------------------------------------------------------------------------------------------
-    function ENT:GetWaterLevel()
-        return ( self:GetAttachmentPoint( "eyes" ).Pos:IsUnderwater() and 3 or self:WorldSpaceCenter():IsUnderwater() and 2 or self:GetPos():IsUnderwater() and 1 or 0 )
-    end
-    ---------------------------------------------------------------------------------------------------------------------------------------------
+	---------------------------------------------------------------------------------------------------------------------------------------------
+	function ENT:GetWaterLevel()
+		return ( self:GetAttachmentPoint( "eyes" ).Pos:IsUnderwater() and 3 or self:WorldSpaceCenter():IsUnderwater() and 2 or self:GetPos():IsUnderwater() and 1 or 0 )
+	end
+	---------------------------------------------------------------------------------------------------------------------------------------------
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
